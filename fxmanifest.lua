844e575c5c1385813fe30695719a6a425a638a9f
Tkiljoy <50683104+Tkiljoy@users.noreply.github.com>
2022-03-28 22:36:00 -0500

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "adamant"
+game "gta5"
+
+author 'TK'
+description 'Most Wanted FiveM'
+version '3.2'
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'mw_photo_2_metadata_001_strm.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'mw_photo_metadata_001_strm.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'mw_veg_metadata_001_strm.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nfsmw_roads_metadata_001_strm.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'worldrcp_metadata_001_strm.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'worldrwd1_metadata_001_strm.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'worldrwd2_metadata_001_strm.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'worldrwd3_metadata_001_strm.ytyp'
